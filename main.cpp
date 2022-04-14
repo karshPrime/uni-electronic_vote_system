879a88a5464ce724665fd023746dd0dc0d1b115f
ut-kr <utkarsh@cyberfear.com>
2022-04-14 11:36:01 +1000

    ✨ input template

--- main.cpp
+++ main.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
 
+template <typename T>
+void input(const std::string& display, T& store_in) {
+  std::cout << display << std::endl;
+  std::cin >> store_in;
+}
+
+
 int main() {
   // 
 }
