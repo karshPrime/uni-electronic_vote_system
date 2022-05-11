f1746596829ba4d557763102e1da472c76786baa
ut-kr <utkarsh@cyberfear.com>
2022-05-11 18:06:46 +1000

    display function fix

--- general.h
+++ general.h
@@ -4,6 +4,8 @@ This header contains declaration to general.cpp functions.
 
 #pragma once            // preventing header from being called more than once
 
-#include "general.cpp"  // general functions required by multiple modules
 #include "options.cpp"  // user option functions
 #include "person.cpp"   // contains voter and candidate class
+
+// print menu and heading
+void print_file_content(std::string file_name);
