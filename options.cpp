664b0fc30a1b591b9419412a73c5dfb1b0cda463
ut-kr <utkarsh@cyberfear.com>
2022-05-12 17:36:32 +1000

    other small lab fixes

--- options.cpp
+++ options.cpp
@@ -2,22 +2,24 @@
 Contains user option functions
 */
 
+#include "general.h"
+
 // would display passed candidate's vote count
-void print_candidate_vote_count(int id) {
+void print_candidate_vote_count(int &id) {
 
 }
 
 // would add votes to required candidate
-void add_vote_to_candidate(int cid, int vid) {
+void add_vote_to_candidate(int &cid, int &vid) {
 
 }
 
 // would display candidate with lowest vote count
-void print_lowest_candidate_votes(int id) {
+void print_lowest_candidate_votes() {
 
 }
 
 // would display candidate with highest vote count
-void print_highest_candidate_votes(int id) {
+void print_highest_candidate_votes() {
 
 }
