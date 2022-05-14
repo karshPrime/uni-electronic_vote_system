3a0773176e98a28e52ac39238ee62ccce0552157
Aweb Mekhilef <awebmekhilef@gmail.com>
2022-05-14 13:43:20 +1000

    print candidate votes

--- options.cpp
+++ options.cpp
@@ -4,15 +4,36 @@
 Contains user option functions
 */
 
+#include <fstream>
+#include <iostream>
+#include <string>
+
 #include "options.h"
+#include "general.h"
 
 // would display passed candidate's vote count
-void print_candidate_vote_count(int &id) {
-	
+void print_candidate_vote_count(int& id) {
+	std::ifstream file("candidates.txt");
+
+	if (file.is_open()) {
+		std::string line;
+		while (std::getline(file, line)) {
+			auto candidate = construct_candidate(line);
+
+			if (candidate.id == id) {
+				std::cout << "Candidate " << id << " has " << candidate.count << " votes." << std::endl;
+				break;
+			}
+		}
+
+		file.close();
+	}
+	else
+		std::cout << "candidate database not found" << std::endl;
 }
 
 // would add votes to required candidate
-void add_vote_to_candidate(int &cid, int &vid) {
+void add_vote_to_candidate(int& cid, int& vid) {
 
 }
 
