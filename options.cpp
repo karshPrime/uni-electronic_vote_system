2c005efd5eaa9424c30fbd45e5562d30d2cb9943
Aweb Mekhilef <awebmekhilef@gmail.com>
2022-05-15 18:06:35 +1000

    added comments for print and add vote options

--- options.cpp
+++ options.cpp
@@ -10,7 +10,7 @@ Contains user option functions
 #include "options.h"
 #include "general.h"
 
-//todo (aweb): check if candidate/voter exists
+//todo (aweb): check if candidate/voter exists (and other validation)
 
 // would display passed candidate's vote count
 void print_candidate_vote_count(int& id) {
@@ -38,51 +38,71 @@ void print_candidate_vote_count(int& id) {
 }
 
 // would add votes to required candidate
-// todo(aweb): clean up and comment this monstrosity
 void add_vote_to_candidate(int& cid, int& vid) {
+	// open the candidate and voter files
 	std::fstream candidateFile("candidates.txt");
 	std::fstream voterFile("voters.txt");
-	std::string tmpStr;
+
+	// this string is used to store the updated file contents (after writing new data)
+	// and is rewritten back to the file
+	std::string updatedFileContents;
 
 	if (candidateFile.is_open()) {
 		std::string line;
 
 		while (std::getline(candidateFile, line)) {
+			// create the candidate given the text string
 			auto candidate = construct_candidate(line);
 
+			// check if this is the candidate we are searching for
 			if (candidate.id == cid) {
+				// increment the candidate votecount
+				// since the vote count is the first token we have to get the index of the first delimeter
+				// and replace the value with the new vote count
 				line.replace(0, line.find(" "), std::to_string(++candidate.count));
-				tmpStr.append(line + "\n");
 			}
+
+			// append the line to the temporary string (whether updated or not)
+			updatedFileContents.append(line + "\n");
 		}
 
-		candidateFile << tmpStr;
+		// write the updated data and close the file
+		candidateFile << updatedFileContents;
 		candidateFile.close();
 	}
 	else
 		std::cout << "candidate database not found" << std::endl;
 
-	tmpStr = "";
+	// reset the string to move on to the voter file
+	updatedFileContents = "";
 
 	if (voterFile.is_open()) {
 		std::string line;
 
 		while (std::getline(voterFile, line)) {
+			// create the voter given the text string
 			auto voter = construct_voter(line);
 
+			// check if this is the voter we are searching for
 			if (voter.id == vid) {
+				// create the timestamp (c++ why you make this so complicated?)
 				std::time_t date = time(0);
 				std::tm tm;
 				localtime_s(&tm, &date);
 
+				// replace the first character (voting status) with y to represent yes
 				line.replace(0, 1, "y");
+
+				// append the date string to end of the line
 				line.append(" " + std::to_string(tm.tm_mday) + "/" + std::to_string(tm.tm_mon + 1) + "/" + std::to_string(tm.tm_year - 100));
 			}
 
-			tmpStr.append(line + "\n");
+			// append the line to the temporary string (whether updated or not)
+			updatedFileContents.append(line + "\n");
 		}
 
-		voterFile << tmpStr;
+		// write the updated data and close the file
+		voterFile << updatedFileContents;
 		voterFile.close();
 	}
 	else
